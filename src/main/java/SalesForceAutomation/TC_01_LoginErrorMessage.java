b0207ce708b46c852589eb1cbf61ba79a5350542
pravallikakonakanchi <pravallika.konakanchi>
2023-03-30 23:26:13 -0700

    updated-login-error-testcase

--- src/main/java/SalesForceAutomation/TC_01_LoginErrorMessage.java
+++ src/main/java/SalesForceAutomation/TC_01_LoginErrorMessage.java
@@ -15,7 +15,9 @@ public class TC_01_LoginErrorMessage {
 		driver.get("https://login.salesforce.com/");
 		driver.manage().window().maximize();
 		Thread.sleep(5000);
+		System.out.println("enter username");
 		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("kpravallika@abc.com");
+		System.out.println("enter blank password");
 		driver.findElement(By.xpath("//input[@id='password']")).clear();
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//input[@id='Login']")).click();
